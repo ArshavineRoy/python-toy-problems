dabbdc32e1010598000d000de5e4e1a67c075ea5
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-18 15:42:28 +0300

    add try & except ValueError for non-integer inputs

--- toy-problems/positive-numbers.py
+++ toy-problems/positive-numbers.py
@@ -2,11 +2,14 @@ def positive_numbers(a, b, c):
     matches = [num for num in [a, b, c]  if num > 0]
     return True if len(matches) == 2 else False
 
+try:
+    print ("Enter inputs for values of a, b, c.")
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+    
+    result = positive_numbers(a, b, c)
+    print(result)
 
-print ("Enter your integer values below")
-a = int(input("a: "))
-b = int(input("b: "))
-c = int(input("c: "))
-
-result = positive_numbers(a, b, c)
-print("Input has only two positive numbers:", result)
+except ValueError:
+    print("Please enter integer values only!")
