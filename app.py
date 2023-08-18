e8956685a5d3a2f3e903d7cc6abdff8a919cb0bf
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-18 21:42:35 +0300

    add time converter label and description

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ class App:
         self.root.geometry("800x500")
         self.root.title("Python Toy Problems")
 
+                # 1. 24-HOUR TIME CONVERTER
+        self.label = tk.Label(self.root, text="Time Converter", font=('Arial', 14, "bold"))
+        self.label.pack(padx=10,pady=10)
+
+        self.description = tk.Label(self.root, text="A time converter that seamlessly converts time from 12-hour format to 24-hour format. \n Please enter time in 12-hour format below, e.g., 12:08 am", font=('Arial', 11))
+        self.description.pack()
+
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
