3c1c35df8a47ff26cc1612820c98af86ecd25d25
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-18 14:13:09 +0300

    add check to handle empty string entry

--- toy-problems/highest-consonant-value.py
+++ toy-problems/highest-consonant-value.py
@@ -1,16 +1,19 @@
 import re
 
 def highest_consonant(string):
-    substrings = re.findall(r"[^aeiou\d\W]+", string.lower())
+    if string:
+        substrings = re.findall(r"[^aeiou\d\W]+", string.lower())
 
-    substring_totals = []
+        substring_totals = []
 
-    for substring in substrings:
-        total_unicode = 0
-        for char in substring:
-            total_unicode += (ord(char) - 96)
-        substring_totals.append(total_unicode)
-    return max(substring_totals)
+        for substring in substrings:
+            total_unicode = 0
+            for char in substring:
+                total_unicode += (ord(char) - 96)
+            substring_totals.append(total_unicode)
+        return print(max(substring_totals))
+    else:
+        return print("You entered an empty string!")
 
-print(highest_consonant(input("Enter a string: ")))
+highest_consonant(input("Enter a string: "))
 
