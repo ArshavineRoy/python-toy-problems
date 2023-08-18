3e91688165a2cd21e33de7518988c8eeca8bd097
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-18 14:01:51 +0300

    loop through substrings and return highest value
        - loop through substrings to get each substring
        - create total_unicode counter
        - loop through each substring to get individial chars (ord () can only
    receive one character at a time)
        - use ord () function to get Unicode char values
        - subtract - 96 to get alphabet position.
            e.g., a = 97 (Unicode) = 1 (alpha).
        - add this value to total_unicode counter
        - append total_unicode to substring_totals
        - return max in the substring_totals list
        - call function with input prompt for string entry & print.

--- toy-problems/highest-consonant-value.py
+++ toy-problems/highest-consonant-value.py
@@ -3,3 +3,14 @@ import re
 def highest_consonant(string):
     substrings = re.findall(r"[^aeiou\d\W]+", string.lower())
 
+    substring_totals = []
+
+    for substring in substrings:
+        total_unicode = 0
+        for char in substring:
+            total_unicode += (ord(char) - 96)
+        substring_totals.append(total_unicode)
+    return max(substring_totals)
+
+print(highest_consonant(input("Enter a string: ")))
+
